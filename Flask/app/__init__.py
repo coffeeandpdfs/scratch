1eb3a954167abc739f122eb8e2de289260d371fe
coffeeandpdfs <mbs5051@gmail.com>
2019-02-05 20:32:37 -0500

    Flask: Updated requirements to include pandas, removed flask_mail from init.

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 bootstrap = Bootstrap()
-mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -16,7 +15,6 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
-    mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
 
