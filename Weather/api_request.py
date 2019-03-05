525c050ca014cfa7e493927fc1695b7ee5a0497e
coffeeandpdfs <mbs5051@gmail.com>
2019-03-05 10:38:03 -0500

    Weather: Retrieve weather data for multiple cities and post to database

--- Weather/api_request.py
+++ Weather/api_request.py
@@ -6,18 +6,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 """
-Perform a get request to the weather API, 
-then load the results in the database
+Perform a get request to the weather API, then load the results in the database
 """
-def main():
-    # Load API key and City_ID from environment variables
-    weather_database = f"{os.path.abspath('.')}\weather.sqlite"
-    weather_api_key = os.getenv('weather_api_key')
-    city_id = os.getenv('weather_api_my_city_id')
+cities = {
+    'Austin': 4671654,
+    'Boston': 4930956,
+    'Chicago': 4887398,
+    'Denver': 5419384,
+    'Erie': 5188843,
+    'New York': 5128581,
+    'Pittsburgh': 5206379,
+    'Raleigh': 4487042,
+    'Tahoe': 5599665
+    }
 
-    # Create the engine to open the connection to the weather DB
-    engine = create_engine(f'sqlite:///{weather_database}', echo=False)
 
+def retrieve_city_data(city_id=None, weather_api_key=None):
     # Get current weather event from API
     r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?id={city_id}&APPID={weather_api_key}')
     weather_event = r.json()
@@ -27,23 +31,48 @@ def main():
     w = insert_weather_event(weather_event)
 
     # Unpack the weather event to create a weather database object
-    newEvent = WeatherAttributes(**w.__dict__)
+    new_weather = WeatherAttributes(**w.__dict__)
+    new_city = City(**c.__dict__)
 
+    return weather_event, new_weather, new_city
+
+
+def commit_data(sesh=None, weather_event=None, new_weather=None, new_City=None):
+    # Check if the current city is in the database, if it isn't, then add it in.
+    city_already_added = sesh.query(City).filter_by(id=weather_event['id']).first()
+    if not city_already_added:
+        sesh.add(new_City)
+    else:
+        sesh.rollback()
+    # Add the weather event into the database, then commit the object
+    sesh.add(new_weather)
+    sesh.commit()
+
+
+def main():
+    # Load API key and City_ID from environment variables
+    weather_database = f"{os.path.abspath('.')}\weather.sqlite"
+    WeatherApiKey = os.getenv('weather_api_key')
+    #city_id = os.getenv('weather_api_my_city_id')
+
+    # Create the engine to open the connection to the weather DB
+    engine = create_engine(f'sqlite:///{weather_database}', echo=False)
     # Create a session with the database
     Session = sessionmaker()
     Session.configure(bind=engine)
     session = Session()
 
-    # Check if the current city is in the database, if it isn't, then add it in.
-    city_already_added = session.query(City).filter_by(id=weather_event['id']).first()
-    if not city_already_added:
-        newCity = City(**c.__dict__)
-        session.add(newCity)
-    else:
-        session.rollback()
-    # Add the weather event into the database, then commit the object
-    session.add(newEvent)
-    session.commit()
+    for city, cityid in cities.items():
+        print(f"Starting API GET request for {city}")
+        try:
+            newEvent, newWeather, newCity = retrieve_city_data(cityid, WeatherApiKey)
+        except Exception as e:
+            print(f'\t{e}')
+        print(f"Starting SQL calls for {city}")
+        try:
+            commit_data(session, newEvent, newWeather, newCity)
+        except Exception as e:
+            print(f'\t{e}')
 
 
 if __name__ == "__main__":
--- Weather/dbmodels.py
+++ Weather/dbmodels.py
@@ -16,7 +16,7 @@ engine = create_engine(f'sqlite:///{weather_database}', echo=False)
 Base = declarative_base()
 
 
-# Create Python data classes to make it easier/cleaner to put data 
+# Create Python data classes to make it easier/cleaner to put data
 # into SQLAlchemy objects.  Currently have a City and Weather Attribute class
 """
 Database Objects
@@ -24,21 +24,19 @@ Database Objects
 @dataclass
 class City(Base):
     __tablename__ = 'city'
-    
-    #id = Column(Integer, Sequence('city_id_seq'), primary_key=True)
+    # id = Column(Integer, Sequence('city_id_seq'), primary_key=True)
     id = Column(Integer, primary_key=True)
     city_name = Column(String(50))
     country_name = Column(String(50))
     longitude = Column(Float)
     latitude = Column(Float)
 
+
 @dataclass
 class WeatherAttributes(Base):
     __tablename__ = 'weatherattributes'
-    
     id = Column(Integer, Sequence('weather_id_seq'), primary_key=True)
     city_id = Column(Integer, ForeignKey("city.id"))
-    
     dt = Column(Integer)
     sunrise_dt = Column(Integer)
     sunset_dt = Column(Integer)
@@ -72,7 +70,8 @@ class insert_city:
         self.country_name = weather_dict['sys']['country']
         self.longitude = weather_dict['coord']['lon']
         self.latitude = weather_dict['coord']['lat']
-    
+
+ 
 @dataclass
 class insert_weather_event:
     """
@@ -100,8 +99,6 @@ class insert_weather_event:
             self.rain_1h = weather_dict.get('rain').get('1h')
             self.rain_3h = weather_dict.get('rain').get('3h')
 
-
-#Base.metadata.create_all(engine)
 
 # Run this .py script to create the database if it doesn't already exist
 def main():
